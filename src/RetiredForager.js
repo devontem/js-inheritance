1770dd1aa7d640206d37cea5d5341a02b0936379
Devonte M <devontem@live.com>
2016-01-18 15:12:26 -0800

    Completed JS Classes

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,16 @@
 var RetiredForagerBee = function() {
+	ForagerBee.call(this);
+	this.age = 40;
+	this.job = 'gamble';
+	this.canFly = false;
+	this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.gamble = this.forage;
+
+RetiredForagerBee.prototype.forage = function(){
+	returns "I am too old, let me play cards instead";
+}
